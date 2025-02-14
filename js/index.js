3b15c947ec35bd274a2c9d3a99d27849f7f84ffe
anouther <155570107+anouther@users.noreply.github.com>
2025-02-14 15:21:32 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'HAII MAAIKE LUNGS.'.split('')
+const text = 'Haii Kai'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
