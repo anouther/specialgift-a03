6d68085859789a4b6734ebb01f8f284107107049
anouther <155570107+anouther@users.noreply.github.com>
2026-05-01 20:25:48 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('I Love You. ').split('');
+        const titles = ('For My Precious Wife').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Happy 2-monthiversary to the most amazing Halston! Time flies when you're having fun, and I'm definitely having a blast with you. Thanks for making these past two months so awesome. Let's keep the good times rolling!";
+            subtitle.innerText = "Aku ga pernah nyangka waktu bisa jalan secepet ini.. and i didn’t expect that i’d spend these past 4 months with u by my side. Aku bersyukur banget atas semua yang kamu kasih ke aku, apapun itu. I feel really lucky to have u, and i still want u for as long as i can. Selama kita masih saling sayang dan masih mau berusaha buat memperbaiki apapun yang datang ke kita, let’s keep going, together. I hope you’ll keep choosing me as your partner, just like how i always choose u. Aku selalu menghargai setiap percakapan kita, setiap momen kecil yang kita lewatin. I’m really happy we made it this far, and we’re still loving each other sampe sekarang. I hope we can stay like this, and go even further… maybe to 5 months and more, ya sayang? I Love You The Most, Babyyy. Happy 4 Months 🤍";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
